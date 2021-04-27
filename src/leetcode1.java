37bb8f033318a86fb4e2eb5d1ab26847519d586b
applevvn <nishengri>
2021-04-28 01:11:05 +0800

    leetcode1

--- src/leetcode1.java
+++ src/leetcode1.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+//c
 public class leetcode1 {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
