d01043e549a5242bbad3b9b174bb7eb505923a4f
applevvn <nishengri>
2021-04-28 01:02:54 +0800

    leetcode1

--- src/leetcode.java
+++ src/leetcode.java
@@ -1,5 +1,12 @@
+
 public class leetcode {
     public static void main(String[] args) {
         System.out.println("hello leetcode");
+        leetcode1 leetcode1 = new leetcode1();
+        int[] arr = {2,7,11,15};
+        int[] ints = leetcode1.twoSum(arr, 9);
+        for (int i : ints) {
+            System.out.println(i);
+        }
     }
 }
---
+++ src/leetcode1.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class leetcode1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int firstNum = target - nums[i];
+            if (map.containsKey(firstNum)) {
+                return new int[]{i, map.get(firstNum)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+}
+
